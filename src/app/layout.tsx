5e85609a6c3d6ceb4ce52c0b614db4ac84f1601b
Rayy Summers <106456682+RayySummers@users.noreply.github.com>
2026-07-17 21:11:24 +0800

    Fix fonts: use Chinese mirror fonts.font.im, add missing CSS vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,9 +47,9 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" data-theme="dark" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Noto+Sans+SC:wght@400;500;700&family=Noto+Serif+SC:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.font.im" />
+        <link rel="preconnect" href="https://cdn.font.im" crossOrigin="anonymous" />
+        <link href="https://fonts.font.im/css2?family=Newsreader:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Noto+Sans+SC:wght@400;500;700&family=Noto+Serif+SC:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body>
         <LenisProvider>
